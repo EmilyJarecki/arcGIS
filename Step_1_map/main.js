e3b02b1abf90a4c8012aaebb125d0055ab3a6993
EmilyJarecki <jareckiemily@yahoo.com>
2023-05-27 19:27:00 -0500

    popup works

--- Step_1_map/main.js
+++ Step_1_map/main.js
@@ -10,11 +10,40 @@ require([
   const url =
     "https://services9.arcgis.com/q5uyFfTZo3LFL04P/arcgis/rest/services/survey123_0954ef4c3eb74d9989a91330c7740a9f/FeatureServer/0";
 
+// Popup template
+const template = {
+  title: "{Name}",
+  lastEditInfoEnabled: false,
+  content: [{
+    type: "fields",
+    fieldInfos: [{
+      fieldName: "Address",
+      label: "Address"
+    },{
+      fieldName: "Industry",
+      label: "Industry"
+    }]
+  },{
+    type: "text",
+    text: '<b>{expression/has-website}</b> <a href={expression/website-expr}>{expression/website-expr}</a>'
+  }],
+  expressionInfos: [{
+    name: 'website-expr',
+    title: 'Website: ',
+    expression: "IIF(!IsEmpty($feature.Website), $feature.Website, null)"
+  },{
+    name: 'has-website',
+    expression: 'IIf(!IsEmpty($feature.Website), "Website: ", "No website found for this business")' 
+  }]
 
+}
+
+// Initializing FeatureLayer
   const featureLayer = new FeatureLayer({
     title: "Black-owned Businesses",
     url: url,
     copyright: "BGMAPP",
+    popupTemplate: template //pass the popup
   });
 
   const map = new Map({
